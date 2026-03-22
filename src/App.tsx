d3d79ef29c069dbbd060f18f8fbcc9b7f1a5fa1f
Ramerlin <ramerlint2008@gmail.com>
2026-03-22 19:53:04 -0400

    feat: add @vercel/analytics integration to track application usage

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import { Analytics } from "@vercel/analytics/next"
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
+          <Analytics />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
